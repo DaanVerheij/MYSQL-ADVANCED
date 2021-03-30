ae1023373bede40a62262b97dafbec553d92894b
Daan <daan.verheij@student.rocva.nl>
2021-03-30 12:06:11 +0200

    taak 02

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, geboortedatum, woonplaats)
+VALUES (11, 'Marco', 'Van Basten', '2000-03-22', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam, achternaam, geboortedatum, woonplaats)
+VALUES (12, 'Dennis', 'Bergkamp', '2002-03-27', 'Amsterdam')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, geboortedatum, woonplaats)
+VALUES (13, 'Frank', 'Rijkaard', '2001-02-28', 'Amsterdam')
