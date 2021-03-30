fb0025c654ad02e9c2965c4b089a5e9affd781cb
Daan <daan.verheij@student.rocva.nl>
2021-03-30 11:50:51 +0200

    taak 01

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,19 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES (5, 'Kees', 'Janssen', '9A', 'Zwaantjesstraat', '1100AX', '2001-5-22', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES (6, 'Jan', 'De wit', '9A', 'Hondenlaan', '1155WF', '2000-07-02', 'Amsterdam')
+
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES (7, 'Klaasjan', 'Huntelaar', '0A', 'Kattenlaan', '1186WX', '2000-09-12', 'Amsterdam')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES (8, 'Robin', 'Van Persie', '0A', 'Appelaan', '1166AX', '2000-09-12', 'Amsterdam')
+
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats ) 
+VALUES (9, 'Wesley', 'Sneijder', '5A', 'Perenlaan', '1186AX', '2001-07-12', 'Amsterdam')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats )
+VALUES (10, 'Patrick', 'Kluivert', '9A', 'Herenstraat', '1122WX', '2002-12-02', 'Amsterdam')
